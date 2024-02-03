e00eb47392f1657c1339d76f04180434146667a3
lijund2011 <77002051+lijund2011@users.noreply.github.com>
2024-02-03 14:59:15 +0800

    Add files via upload

---
+++ ip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ 
+if [! -f /root/ip.txt];then
+echo $(date +%Y"."%m"."%d" "%k":"%M":"%S) > /root/ip.txt
+curl http://members.3322.org/dyndns/getip >> /root/ip.txt
+fi
+ 
+while var=$(curl http://members.3322.org/dyndns/getip) || true; do
+ 
+iptxt=$(tail -1 /root/ip.txt)
+#echo $iptxt
+#echo $var
+ 
+if [ $var != $iptxt ];then
+ 
+echo $(date +%Y"."%m"."%d" "%k":"%M":"%S) >> /root/ip.txt
+ 
+echo "$var" >> /root/ip.txt
+ 
+#echo "record ip"
+ 
+#else
+ 
+#echo "ip is same"
+ 
+fi
+ 
+sleep 60s
+done
