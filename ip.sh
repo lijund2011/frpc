05eed1d1902f02a14c24a5cc216113bb007c2a44
lijund2011 <77002051+lijund2011@users.noreply.github.com>
2024-02-03 15:57:05 +0800

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 url="http://example.com/api" # 替换为要访问的接口URL
-filename="response.txt"      # 指定保存结果的文件名
+filename="/response.txt"      # 指定保存结果的文件名
  
 curl -s "$url" > $filename   # 通过curl命令发送GET请求并将返回的内容保存到文件中
 echo "已成功保存接口内容到$filename文件中！"
